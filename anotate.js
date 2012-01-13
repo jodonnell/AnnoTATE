db88723927ea6c78718dfb21d05cc0d36ece9527
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-01-12 20:58:22 -0500

    you can turn it on and off

--- anotate.js
+++ anotate.js
@@ -1,16 +1,8 @@
-var width = $(document).width();
-var height = $(document).height();
-$('body').append('<canvas id="drawCanvas" width="' + width + '" height="' + height + '"></canvas>');
-$("#drawCanvas").css('position', 'absolute');
-$("#drawCanvas").css('top', '0px');
-$("#drawCanvas").css('left', '0px');
-$("#drawCanvas").css('z-index', '1000000000');
-//$("#drawCanvas").css('pointer-events', 'none');
-
-
-
 var Draw = Class.extend({
     init: function() {
+	this.createCanvas();
+	this.hideCanvas();
+
 	this.buttonDepressed = false;
 	this.lastX = 0;
 	this.lastY = 0;
@@ -18,6 +10,34 @@ var Draw = Class.extend({
 	this.mouseDown();
 	this.mouseUp();
 	this.mouseMove();
+	this.keyDown();
+    },
+
+    createCanvas: function() {
+	var width = $(document).width();
+	var height = $(document).height();
+	$('body').append('<canvas id="drawCanvas" width="' + width + '" height="' + height + '"></canvas>');
+	$("#drawCanvas").css('position', 'absolute');
+	$("#drawCanvas").css('top', '0px');
+	$("#drawCanvas").css('left', '0px');
+	$("#drawCanvas").css('z-index', '1000000000');
+    },
+
+    hideCanvas: function() {
+	$("#drawCanvas").css('pointer-events', 'none');
+	this.hidden = true;
+    },
+
+    showCanvas: function() {
+	$("#drawCanvas").css('pointer-events', 'auto');
+	this.hidden = false;
+    },
+
+    toggleCanvas: function() {
+	if (this.hidden)
+	    this.showCanvas();
+	else
+	    this.hideCanvas();
     },
 
     mouseDown: function() {
@@ -47,6 +67,16 @@ var Draw = Class.extend({
 
 	    this.lastX = e.pageX;
 	    this.lastY = e.pageY;
+	});
+    },
+
+    keyDown: function() {
+	var me = this;
+	$('body').on('keydown', function(e) {
+            code = (e.keyCode ? e.keyCode : e.which);
+	    if (code === 65 && e.altKey) { // alt-a
+		me.toggleCanvas();
+	    }
 	});
     }
 });
